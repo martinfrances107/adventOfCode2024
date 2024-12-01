4dae152f531b251c75285911ef38a17d1d353c8b
martinfrances107 <martinfrances107@hotmail.com>
2024-12-01 07:11:04 +0000

    Passes part2 now have 2 gold stars.

---
+++ day1/src/bin/part2.rs
@@ -0,0 +1,58 @@
+use nom::bytes::complete::tag;
+use nom::character::complete::digit1;
+
+use nom::combinator::{map, map_res};
+use nom::{sequence::tuple, IResult};
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    println!("{:?}", part2(input));
+}
+
+fn parse_value(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, str::parse)(input)
+}
+
+fn parse_pair(input: &str) -> IResult<&str, (u32, u32)> {
+    map(
+        tuple((parse_value, tag("   "), parse_value)),
+        |(a, _, b)| (a, b),
+    )(input)
+}
+
+fn part2(input: &str) -> u32 {
+    let mut left = vec![];
+    let mut right = vec![];
+    for line in input.lines() {
+        let (_remain, (a, b)) = parse_pair(line).expect("could not parse line");
+        left.push(a);
+        right.push(b)
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut totals = vec![];
+    for query in left {
+        // filter right hand side and count
+        let c = right.iter().filter(|x| **x == query).count();
+        totals.push(query * c as u32);
+    }
+    totals.iter().sum()
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    #[test]
+    fn example() {
+        let input = r"3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!(part1(input), 11u32);
+    }
+}
