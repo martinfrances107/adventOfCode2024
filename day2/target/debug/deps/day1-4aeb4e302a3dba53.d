b6c4c30f0df0c5764f5f2c31ed238a40042abe03
martinfrances107 <martinfrances107@hotmail.com>
2024-12-02 06:16:53 +0000

    day2p2 - Got the star.

---
+++ day2/target/debug/deps/day1-4aeb4e302a3dba53.d
@@ -0,0 +1,10 @@
+/home/martin/build/adventOfCode2024/day2/target/debug/deps/libday1-4aeb4e302a3dba53.rmeta: src/main.rs src/./input1.txt Cargo.toml
+
+/home/martin/build/adventOfCode2024/day2/target/debug/deps/day1-4aeb4e302a3dba53.d: src/main.rs src/./input1.txt Cargo.toml
+
+src/main.rs:
+src/./input1.txt:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
