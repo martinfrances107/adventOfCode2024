784bfe158cee931cbc2eeb05322174e879d8a814
martinfrances107 <martinfrances107@hotmail.com>
2024-12-05 19:45:58 +0000

    day2p2 removed target directory.

--- day2/target/release/day1.d
+++
@@ -1,1 +0,0 @@
-/home/martin/build/adventOfCode2024/day2/target/release/day1: /home/martin/build/adventOfCode2024/day2/src/input1.txt /home/martin/build/adventOfCode2024/day2/src/main.rs
--- day2/target/release/deps/day1-5f884a249da4fba8.d
+++
@@ -1,6 +0,0 @@
-/home/martin/build/adventOfCode2024/day2/target/release/deps/day1-5f884a249da4fba8: src/main.rs src/./input1.txt
-
-/home/martin/build/adventOfCode2024/day2/target/release/deps/day1-5f884a249da4fba8.d: src/main.rs src/./input1.txt
-
-src/main.rs:
-src/./input1.txt:
