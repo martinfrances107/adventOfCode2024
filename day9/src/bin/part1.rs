7ab7fe770d0058b3a629191b363fcfe65cbc7b7e
martinfrances107 <martinfrances107@hotmail.com>
2024-12-09 16:36:06 +0000

    Day9p1 Changed algorithm to swap never pop.
    Added unit tests to verify both the long for
    and short form shuffling. Still no change in output!

--- day9/src/bin/part1.rs
+++ day9/src/bin/part1.rs
@@ -1,9 +1,13 @@
-use core::panic;
-
 static CHAR_DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 fn main() {
     let input = include_str!("./input1.txt");
+
+    let len = input.len();
+    println!("size of input {len}");
+    let last = input.chars().last().expect("must have a least of char");
+    println!("last char {last}");
+
     println!("{:?}", part1(input));
 }
 
@@ -31,12 +35,10 @@ fn checksum(input: &[char]) -> u64 {
         .sum()
 }
 
-fn reorder(line: &mut Vec<char>) -> bool {
+fn reorder(line: &mut [char]) -> bool {
     let len = line.len();
     for _ in 0..len {
-        if shuffle(line) {
-            line.pop();
-        } else {
+        if !shuffle(line) {
             return true;
         }
     }
@@ -91,7 +93,7 @@ fn generate_disc_map(input: &str) -> Vec<char> {
 
 fn shuffle(input: &mut [char]) -> bool {
     let first_blank = input.iter().position(|x| *x == '.');
-    let last_num = input.iter().rposition(|x| *x != '.');
+    let last_num = input.iter().rposition(|x| CHAR_DIGITS.contains(x));
     match (first_blank, last_num) {
         (Some(first), Some(last)) => {
             if first == last {
@@ -108,7 +110,7 @@ fn shuffle(input: &mut [char]) -> bool {
         (None, Some(_last)) => {
             // shuffling complete.
             // all dots removed.
-            false
+            panic!("no longer good");
         }
         (Some(_first), None) => {
             panic!("bad failure mode");
@@ -142,29 +144,94 @@ mod test {
     }
 
     #[test]
-    fn shuffle09() {
-        let mut line = r"00...111...2...333.44.5555.6666.777.888899"
-            .chars()
-            .collect::<Vec<char>>();
-        assert!(shuffle(&mut line));
-        let expected = "009..111...2...333.44.5555.6666.777.88889."
-            .chars()
-            .collect::<Vec<char>>();
-        assert_eq!(line, expected);
+    fn shuffle_short() {
+        let steps = [
+            "0..111....22222",
+            "02.111....2222.",
+            "022111....222..",
+            "0221112...22...",
+            "02211122..2....",
+            "022111222......",
+        ];
+
+        for pair in steps.windows(2) {
+            match pair {
+                [first, second] => {
+                    let mut input = first.chars().collect::<Vec<_>>();
+                    let expected = second.chars().collect::<Vec<_>>();
+                    let did_shuffle = shuffle(&mut input);
+                    assert!(did_shuffle);
+                    println!("input {:#?}", &input);
+                    assert_eq!(input, expected);
+                }
+                [_one] => {
+                    panic!("mut get a pair of steps");
+                }
+                [] => {
+                    panic!("mut get more than one");
+                }
+                _ => {
+                    panic!("more than 2")
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn shuffle_long() {
+        let steps = [
+            "00...111...2...333.44.5555.6666.777.888899",
+            "009..111...2...333.44.5555.6666.777.88889.",
+            "0099.111...2...333.44.5555.6666.777.8888..",
+            "00998111...2...333.44.5555.6666.777.888...",
+            "009981118..2...333.44.5555.6666.777.88....",
+            "0099811188.2...333.44.5555.6666.777.8.....",
+            "009981118882...333.44.5555.6666.777.......",
+            "0099811188827..333.44.5555.6666.77........",
+            "00998111888277.333.44.5555.6666.7.........",
+            "009981118882777333.44.5555.6666...........",
+            "009981118882777333644.5555.666............",
+            "00998111888277733364465555.66.............",
+            "0099811188827773336446555566..............",
+        ];
+
+        for pair in steps.windows(2) {
+            match pair {
+                [first, second] => {
+                    let mut input = first.chars().collect::<Vec<_>>();
+                    let expected = second.chars().collect::<Vec<_>>();
+                    let did_shuffle = shuffle(&mut input);
+                    assert!(did_shuffle);
+                    println!("input {:#?}", &input);
+                    assert_eq!(input, expected);
+                }
+                [_one] => {
+                    panic!("mut get a pair of steps");
+                }
+                [] => {
+                    panic!("mut get more than one");
+                }
+                _ => {
+                    panic!("more than 2")
+                }
+            }
+        }
     }
 
     #[test]
     fn reorder_check() {
         let line = "12345";
         let mut dm = generate_disc_map(line);
-        let expected = "022111222.".chars().collect::<Vec<_>>();
+        let expected = "022111222......".chars().collect::<Vec<_>>();
         assert!(reorder(&mut dm));
         assert_eq!(dm, expected);
 
         let mut line = "00...111...2...333.44.5555.6666.777.888899"
             .chars()
             .collect::<Vec<_>>();
-        let expected = "0099811188827773336446555566..".chars().collect::<Vec<_>>();
+        let expected = "0099811188827773336446555566.............."
+            .chars()
+            .collect::<Vec<_>>();
         assert!(reorder(&mut line));
         assert_eq!(line, expected);
     }
